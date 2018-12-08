5fa5fdd65b12b2013ebaae355ed24458d3c20dae
BolunZhu <1870759078@qq.com>
2018-12-08 20:18:50 +0800

    init

---
+++ step1/GBNRdtSender.h
@@ -0,0 +1,14 @@
+#include "RdtSender.h"
+
+class GBNRdtSender : public RdtSender{
+    private:
+
+    public:
+    //构造 析构函数
+    GBNRdtSender();
+    ~GBNRdtSender();
+    //接口函数
+    bool getWaitingState();
+    //
+
+};
