dc244ef1242724301e89d7dfe11872bc02241e89
BolunZhu <1870759078@qq.com>
2018-12-10 18:01:33 +0800

    -ok GBN no-debug info version

---
+++ step1/GBNRdtReceiver.h
@@ -0,0 +1,40 @@
+#include "Global.h"
+#include "string"
+class GBNRDTReceiver: public RdtReceiver{
+    private:
+    unsigned int base;
+    Packet last_ackpkt;
+    public:
+    GBNRDTReceiver();
+    ~GBNRDTReceiver();
+    void receive(Packet &packet);		//接收报文，将被NetworkService调用	
+};
+GBNRDTReceiver::GBNRDTReceiver():base(0)
+{
+    last_ackpkt.acknum=-1;//if 1st pck lost then resend with acknum = -1
+    last_ackpkt.checksum=0;
+    last_ackpkt.seqnum=-1;//unuse
+    for (int i = 0; i < Configuration::PAYLOAD_SIZE; i++){
+		last_ackpkt.payload[i] = '-';
+	}
+    last_ackpkt.checksum=pUtils->calculateCheckSum(last_ackpkt);
+}
+void GBNRDTReceiver::receive(Packet &packet){
+    
+    if (packet.checksum == pUtils->calculateCheckSum(packet) && packet.seqnum==base ) {
+        // not_corrupt && is_expected_ACK
+        // give it to up-layer
+        Message msg;
+        memcpy(msg.data,packet.payload,sizeof(packet.payload));
+        pns->delivertoAppLayer(RECEIVER,msg);
+        // update last_ackpkt
+        last_ackpkt.acknum=packet.seqnum;
+        last_ackpkt.checksum=pUtils->calculateCheckSum(last_ackpkt);
+        pns->sendToNetworkLayer(SENDER,last_ackpkt);
+    }
+    else {
+        //if not correct then resend last ackpkt
+        pns->sendToNetworkLayer(RECEIVER,last_ackpkt);
+    }
+    
+}
--- step1/GBNRdtSender.h
+++ step1/GBNRdtSender.h
@@ -1,12 +1,15 @@
 #include "Global.h"
+#include "string"
 class GBNRdtSender : public RdtSender{
     private:
     static const unsigned int K = 3;//3bit for seq
+    static const unsigned int K_range=1<<K ; //K_range = 2^K
     static const unsigned int N = 4;//window_len = 4
     unsigned int base ;
     unsigned int nextnum ;
-    unsigned int _base(){return base&(7);};
-    unsigned int _nextnum(){return nextnum&(7);};
+    unsigned int _base(){return base&(K_range-1);};
+    unsigned int _nextnum(){return nextnum&(K_range-1);};
+    unsigned int _nextcache(){return _nextnum()&(N-1);};
     Packet pkt[N];
     public:
     //构造 析构函数
@@ -32,9 +35,43 @@ bool GBNRdtSender::send(Message &message) {
     if(getWaitingState()){//full
         return false;
     }
-    //_nextnum()&3 = nextnum in N
-    pkt[_nextnum()&3].acknum=base;
-    pkt[_nextnum()&3].checksum=pUtils->calculateCheckSum(pkt[_nextnum()&3]);
-    pkt[_nextnum()&3].seqnum=_nextnum();
-    pns->sendToNetworkLayer(SENDER,pkt[_nextnum()]);
+    //_nextnum()&(N-1) = nextnum in N
+    pkt[_nextcache()].acknum=-1;//ignore this
+    pkt[_nextcache()].checksum=0;
+    pkt[_nextcache()].seqnum=_nextnum();
+    memcpy(pkt[_nextcache()].payload,message.data,sizeof(message.data));
+    pkt[_nextcache()].checksum=pUtils->calculateCheckSum(pkt[_nextcache()]);
+    
+    pns->sendToNetworkLayer(SENDER,pkt[_nextcache()]);
+    if(_base()==_nextnum())
+    {
+            pns->startTimer(SENDER,Configuration::TIME_OUT,0);
+    }
+    nextnum++;
+    return true;
+}
+void GBNRdtSender::receive(Packet &ackPkt){
+    int checksum = pUtils->calculateCheckSum(ackPkt);
+    
+    if (checksum == ackPkt.checksum && ackPkt.acknum >= _base() ) {
+        base=ackPkt.acknum+1;
+        pns->stopTimer(SENDER,0);
+        //if all send out then stop timer
+        //else restart timer
+        if(_base()!=_nextnum()){
+            pns->startTimer(SENDER,Configuration::TIME_OUT,0);
+        }
+    }
+    else {
+        // do nothing
+    }
+}
+void GBNRdtSender::timeoutHandler(int seqNum){
+    //time out then restart timer
+    pns->stopTimer(SENDER,0);
+    pns->startTimer(SENDER,Configuration::TIME_OUT,0);
+    //resend all
+    for(unsigned int i=_base();i!=_nextnum();i++){
+        pns->sendToNetworkLayer(SENDER,pkt[i&(N-1)]);
+    }
 }
